8684c7d226245f3b19b315fb19dc16dcf13d8ef2
nabetama <mao.nabeta@gmail.com>
2020-01-15 00:42:38 +0900

    common equals()

--- tdd-by-example/src/main/java/guru/springframework/Franc.java
+++ tdd-by-example/src/main/java/guru/springframework/Franc.java
@@ -1,19 +1,11 @@
 package guru.springframework;
 
-public class Franc {
-  private int amount;
-
+public class Franc extends Money {
   public Franc(int amount) {
     this.amount = amount;
   }
 
   Franc times(int multiplier) {
     return new Franc(amount * multiplier);
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    Franc franc = (Franc) obj;
-    return amount == franc.amount;
   }
 }
---
+++ tdd-by-example/src/main/java/guru/springframework/Money.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+public class Money {
+  protected int amount;
+
+  @Override
+  public boolean equals(Object obj) {
+    Money money = (Money) obj;
+    return amount == money.amount;
+  }
+}
