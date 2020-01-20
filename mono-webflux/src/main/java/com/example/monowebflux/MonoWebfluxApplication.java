a762cb7e19abdcfcf6c03be950f9d6cb49dc6057
nabetama <mao.nabeta@gmail.com>
2020-01-20 22:17:38 +0900

    spring initializer

---
+++ mono-webflux/src/main/java/com/example/monowebflux/MonoWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.monowebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonoWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonoWebfluxApplication.class, args);
+	}
+
+}
