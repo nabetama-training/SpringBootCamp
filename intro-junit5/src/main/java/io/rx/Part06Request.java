a51121d5fd79f32bd57579cd445cd20dc6f40b02
nabetama <mao.nabeta@gmail.com>
2020-02-02 23:56:30 +0900

    Part06

---
+++ intro-junit5/src/main/java/io/rx/Part06Request.java
@@ -0,0 +1,34 @@
+package io.rx;
+
+import io.rx.domain.User;
+import io.rx.repository.ReactiveRepository;
+import io.rx.repository.ReactiveUserRepository;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+import reactor.test.StepVerifier.Step;
+
+public class Part06Request {
+
+  ReactiveRepository<User> repository = new ReactiveUserRepository();
+
+  public StepVerifier requestAllExpectFour(Flux<User> flux) {
+    return StepVerifier.create(flux)
+        .expectNextCount(4)
+        .expectComplete();
+  }
+
+
+  public StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
+    return StepVerifier.create(flux, 1)
+        .expectNext(User.SKYLER)
+        .thenRequest(1)
+        .expectNext(User.JESSE)
+        .thenCancel();
+  }
+
+  public Flux<User> fluxWithLog() {
+    return repository
+        .findAll()
+        .log();
+  }
+}
