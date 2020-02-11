19eb88f739d10010e85a23e51025cb9c1be279b9
nabetama <mao.nabeta@gmail.com>
2020-02-11 21:41:06 +0900

    Part07Errors

--- intro-junit5/src/main/java/io/rx/Part07Errors.java
+++ intro-junit5/src/main/java/io/rx/Part07Errors.java
@@ -1,6 +1,8 @@
 package io.rx;
 
 import io.rx.domain.User;
+import reactor.core.Exceptions;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class Part07Errors {
@@ -9,4 +11,28 @@ public class Part07Errors {
   public Mono<User> betterCallSaulForBogusMono(Mono<User> error) {
     return error.onErrorResume(e -> Mono.just(User.SAUL));
   }
+
+  public Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
+    return flux.onErrorResume(e -> Flux.just(User.SAUL, User.JESSE));
+  }
+
+  public Flux<User> capitalizeMany(Flux<User> just) {
+    return just.map(
+        user -> {
+          try {
+            return capitalizeUser(user);
+          } catch (CapitalizeError e) {
+            throw Exceptions.propagate(e);
+          }
+        });
+  }
+
+  private User capitalizeUser(User user) throws CapitalizeError {
+    if (user.equals(User.SAUL)) {
+      throw new CapitalizeError();
+    }
+    return new User(user.getName(), user.getFirstname(), user.getLastname());
+  }
+
+  protected final class CapitalizeError extends Exception {}
 }
--- intro-junit5/src/test/java/io/rx/Part07ErrorsTest.java
+++ intro-junit5/src/test/java/io/rx/Part07ErrorsTest.java
@@ -4,6 +4,7 @@ import io.rx.domain.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -19,5 +20,25 @@ public class Part07ErrorsTest {
 
     mono = workshop.betterCallSaulForBogusMono(Mono.just(User.SKYLER));
     StepVerifier.create(mono).expectNext(User.SKYLER).verifyComplete();
+  }
+
+  @Test
+  void fluxWithValueInsteadOfError() {
+    Flux<User> flux = workshop.betterCallSaulAndJesseForBogusFlux(Flux.error(new IllegalStateException()));
+    StepVerifier.create(flux)
+        .expectNext(User.SAUL, User.JESSE)
+        .verifyComplete();
+
+    flux = workshop.betterCallSaulAndJesseForBogusFlux(Flux.just(User.SKYLER, User.WALTER));
+    StepVerifier.create(flux)
+        .expectNext(User.SKYLER, User.WALTER)
+        .verifyComplete();
+  }
+
+  @Test
+  public void handleCheckedExceptions() {
+    Flux<User> flux = workshop.capitalizeMany(Flux.just(User.SAUL, User.JESSE));
+    StepVerifier.create(flux)
+        .verifyError(Part07Errors.CapitalizeError.class);
   }
 }
