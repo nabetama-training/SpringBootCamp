74c5bf6392deac4a1ee91f81f8b9a3059b15e7ba
nabetama <mao.nabeta@gmail.com>
2020-03-02 23:57:08 +0900

    .

--- intro-junit5/src/main/java/io/rx/Part01Flux.java
+++ intro-junit5/src/main/java/io/rx/Part01Flux.java
@@ -1,29 +1,29 @@
 package io.rx;
 
 import java.time.Duration;
+
 import reactor.core.publisher.Flux;
 
 public class Part01Flux {
 
-  Flux<String> emptyFlux() {
-    return Flux.empty();
-  }
+    Flux<String> emptyFlux() {
+        return Flux.empty();
+    }
 
-  Flux<String> fooBarFluxFromValues() {
-    return Flux.just("foo", "bar");
-  }
+    Flux<String> fooBarFluxFromValues() {
+        return Flux.just("foo", "bar");
+    }
 
-  public Flux<String> fooBarFluxFromList() {
-    return Flux.fromArray(new String[]{"foo", "bar"});
-  }
+    public Flux<String> fooBarFluxFromList() {
+        return Flux.fromArray(new String[]{"foo", "bar"});
+    }
 
+    public Flux<String> errorFlux() {
+        return Flux.error(new IllegalStateException());
+    }
 
-  public Flux<String> errorFlux() {
-    return Flux.error(new IllegalStateException());
-  }
-
-  public Flux<Long> counter() {
-    return Flux.interval(Duration.ofMillis(100))
-        .take(10);
-  }
+    public Flux<Long> counter() {
+        return Flux.interval(Duration.ofMillis(100))
+                .take(10);
+    }
 }
